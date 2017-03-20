0db00f5315c34e3959170ee4151f2a69b522ed2f
muxiujh <373259115@qq.com>
2017-03-20 15:17:21 +0800

    add TList

---
+++ WxhnecServer/Logics/Attributes/TListSearch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WxhnecServer.Logics.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class TListSearch : Attribute
+    {
+        public bool Enable { get; set; }
+
+        public TListSearch(bool enable = true) {
+            Enable = enable;
+        }
+    }
+}
---
+++ WxhnecServer/Logics/Attributes/TListShow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WxhnecServer.Logics.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class TListShow : Attribute
+    {
+        public bool Enable { get; set; }
+
+        public TListShow(bool enable = true) {
+            Enable = enable;
+        }
+    }
+}
